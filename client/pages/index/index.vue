2155e584d57d146e822eac08f8c3f34aace883f7
jixingx <1083673246@qq.com>
2020-07-03 23:10:16 +0800

    主页列表布局完成

--- client/pages/index/index.vue
+++ client/pages/index/index.vue
@@ -16,6 +16,55 @@
 				</view>
 			</view>
 		</view>
+		<view class="main">
+			<view class="apply">
+				
+			</view>
+			<view class="friends">
+				<view class="friend-list">
+					<view class="friend-list-l">
+						<text class="tip">1</text>
+						<image src="../../static/logo.png" mode=""></image>
+					</view>
+					<view class="friend-list-r">
+						<view class="top">
+							<view class="name">
+								都知道
+							</view>
+							<view class="time">
+								13:43
+							</view>
+							
+						</view>
+						<view class="news">
+							事物所包含的实质性事物dsfsdfdsfsddsffsdfsdfds
+						</view>
+					</view>
+				</view>
+			</view>
+			<view class="friends">
+				<view class="friend-list">
+					<view class="friend-list-l">
+						<text class="tip">1</text>
+						<image src="../../static/logo.png" mode=""></image>
+					</view>
+					<view class="friend-list-r">
+						<view class="top">
+							<view class="name">
+								都知道
+							</view>
+							<view class="time">
+								13:43
+							</view>
+							
+						</view>
+						<view class="news">
+							事物所包含的实质性事物dsfsdfdsfsddsffsdfsdfds
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -37,31 +86,34 @@
 
 <style lang="scss">
 	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+		// display: flex;
+		// flex-direction: column;
+		// align-items: center;
+		// justify-content: center;
 	}
 	.top-bar{
 		position: fixed;
 		top: 0;
 		left: 0;
+		z-index: 1001;
 		height: 88rpx;
 		width: 100%;
 		box-sizing: border-box;
 		background-color: $uni-bg-color;
-		padding-left: $uni-spacing-col-base;
-		padding-right: $uni-spacing-col-base;
+		//padding-left: $uni-spacing-col-base;
+		//padding-right: $uni-spacing-col-base;
 		// box-shadow: 0px 1px 0px 0px rgba(0,0,0,0.1);
 		background: $uni-bg-color;
 		border-bottom:1px solid $uni-border-color;
 		.top-bar-left{
 			float: left;
+			padding-left: $uni-spacing-col-base;
 			image{
 				padding-top: 10rpx;
 				width: 68rpx;
 				height: 68rpx;
 				border-radius: 16rpx;
+				background-color: $uni-color-primary;
 			}
 		}
 		.top-bar-center{
@@ -80,6 +132,7 @@
 		}
 		.top-bar-right{
 			float: right;
+			padding-right: 14rpx;
 			.search{
 				width: 88rpx;
 				height: 88rpx;
@@ -95,6 +148,65 @@
 				width: 52rpx;
 				height: 52rpx;
 			}
+		}
+	}
+	.main{
+		padding: 88rpx $uni-spacing-col-base 0;
+	}
+	.friend-list{
+		height: 96rpx;
+		padding: 16rpx 0;
+		.friend-list-l{
+			position: relative;
+			float: left;
+			image{
+				width: 96rpx;
+				height: 96rpx;
+				border-radius: $uni-border-radius-base;
+			}
+			.tip{
+				position: absolute;
+				z-index: 10;
+				top: -6rpx;
+				left: 68rpx;
+				min-width: 36rpx;
+				height: 36rpx;
+				background: $uni-color-warning;
+				border-radius: $uni-border-radius-circle;
+				font-size: $uni-font-size-sm;
+				color: $uni-text-color-inverse;
+				line-height: 36rpx;
+				text-align: center;
+			}
+		}
+		.friend-list-r{
+			padding-left:128rpx;
+			
+			.top{
+				height: 50rpx;
+				overflow: hidden;
+				.name{
+					float: left;
+					font-size: 36rpx;
+					color: $uni-text-color;
+					line-height: 50rpx;
+				}
+				.time{
+					float: right;
+					font-size: $uni-font-size-sm;
+					color: $uni-text-color-disable;
+					line-height: 58rpx;
+				}
+			}
+		}
+		.news{
+			font-size: $uni-font-size-base;
+			color: $uni-text-color-grey;
+			line-height: 40rpx;
+			display:-webkit-box;
+			-webkit-box-orient:vertical;
+			-webkit-line-clamp:1;
+			overflow: hidden;
 		}
 	}
 </style>
